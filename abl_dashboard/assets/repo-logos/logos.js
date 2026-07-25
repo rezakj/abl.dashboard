0f79ca5c219a229d26bed7ca975a29f643fd1bcb
Alireza Khodadadi-Jamayran <rezakj@users.noreply.github.com>
2026-07-24 22:14:12 -0400

    ABL dashboard 1st commit
    Added former members. lighter folder.

---
+++ abl_dashboard/assets/repo-logos/logos.js
@@ -0,0 +1,11 @@
+// Official logo URLs with local SVG fallbacks for offline viewing
+window.REPO_LOGO_SOURCES = [
+  { key: 'pcawg', label: 'PCAWG PanCancer', href: 'https://dcc.icgc.org/pcawg/', src: 'https://docs.icgc.org/pcawg/img/pcawg-logo.png', fallback: 'assets/repo-logos/pcawg.svg' },
+  { key: 'tcga', label: 'The Cancer Genome Atlas (TCGA)', href: 'https://www.cancer.gov/tcga', src: 'https://gdc.cancer.gov/files/public/image/tcga_logo.png', fallback: 'assets/repo-logos/tcga.svg' },
+  { key: 'encode', label: 'ENCODE', href: 'https://www.encodeproject.org/', src: 'https://www.encodeproject.org/static/img/encode-logo.png', fallback: 'assets/repo-logos/encode.svg' },
+  { key: 'cbioportal', label: 'cBioPortal', href: 'https://www.cbioportal.org/', src: 'https://www.cbioportal.org/images/cbioportal_logo.png', fallback: 'assets/repo-logos/cbioportal.svg' },
+  { key: 'ucsc', label: 'UCSC Genome Browser', href: 'https://genome.ucsc.edu/', src: 'https://genome.ucsc.edu/images/UCSC_logo_2022.png', fallback: 'assets/repo-logos/ucsc.svg' },
+  { key: 'tracerx', label: 'TRACERx', href: 'https://www.tracerx.co.uk/', src: 'https://www.tracerx.co.uk/wp-content/uploads/2019/03/TRACERx-logo.png', fallback: 'assets/repo-logos/tracerx.svg' },
+  { key: 'icgc', label: 'International Cancer Genome Consortium (ICGC)', href: 'https://icgc.org/', src: 'https://dcc.icgc.org/images/icgc-logo.png', fallback: 'assets/repo-logos/icgc.svg' },
+  { key: 'cruk', label: 'Cancer Research UK', href: 'https://www.cancerresearchuk.org/', src: 'https://www.cancerresearchuk.org/sites/all/themes/custom/cruk/logo.svg', fallback: 'assets/repo-logos/cruk.svg' },
+];
